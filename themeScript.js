db474570268b471508525fd38e83a68df9a7038b
RogerWetter <weatherroger@gmail.com>
2024-08-19 16:08:20 +0200

    nicer toggle

--- themeScript.js
+++ themeScript.js
@@ -16,18 +16,24 @@ function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark })
 
 function updateThemeOnHtmlEl({ theme }) {
   document.querySelector("html").setAttribute("data-theme", theme);
+
+  const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+  if (theme === "dark") {
+    metaThemeColor.setAttribute("content", "#000000");
+  } else {
+    metaThemeColor.setAttribute("content", "#FAEBD7");
+  }
 }
 
-
 document.addEventListener("DOMContentLoaded", function() {
-  const button = document.getElementById("theme-toggle-case")
+  const button = document.getElementById("theme-toggle-case");
   const localStorageTheme = localStorage.getItem("theme");
   const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-// get current Theme
+  // Get current theme
   let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
 
-// Update the theme setting and button text accoridng to current settings
+  // Update the theme setting and button text according to current settings
   updateThemeOnHtmlEl({ theme: currentThemeSetting });
 
   if (button) {
