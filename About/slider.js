94db9d141d62549a0b01a5bb5f9023d936f1733c
RogerWetter <weatherroger@gmail.com>
2024-08-22 10:47:23 +0200

    Slider in about

--- About/slider.js
+++ About/slider.js
@@ -1,28 +1,15 @@
 let slideIndex = 1;
+const slides = document.getElementsByClassName("mySlides");
 showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  if (slides.length < 1) return
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  Array.from(slides).forEach(slide => slide.style.display = "none");
+  slides[slideIndex - 1].style.display = "block";
 }
